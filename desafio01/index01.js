67fd4d5b43aebd7d114877edef30b6e094a4bdc7
Matheus Da Silva Gastaldi <matheus.30220@alunosatc.edu.br>
2025-09-17 19:31:35 -0300

    Primeira questao da prova feita

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,100 @@
-console.log("Hello, World! 001");
+class Lanche {
+    constructor() {
+      this.pao = null;
+      this.carne = null;
+      this.queijo = null;
+      this.salada = null;
+      this.molho = null;
+    }
+  
+    show() {
+      console.log("Lanche:", {
+        pao: this.pao,
+        carne: this.carne,
+        queijo: this.queijo,
+        salada: this.salada,
+        molho: this.molho,
+      });
+    }
+}
+
+class LancheBuilder {
+    constructor() {
+        this.lanche = new Lanche();
+    }
+
+    setPao(pao) {
+        this.lanche.pao = pao;
+        return this;
+    }
+
+    setCarne(carne) {
+        this.lanche.carne = carne;
+        return this;
+    }
+
+    setQueijo(queijo) {
+        this.lanche.queijo = queijo;
+        return this;
+    }
+
+    setSalada(salada) {
+        this.lanche.salada = salada;
+        return this;
+    }
+
+    setMolho(molho) {
+        this.lanche.molho = molho;
+        return this;
+    }
+
+    build() {
+        return this.lanche;
+    }
+}
+  
+//const lancheSimples = new Lanche(true, true, false, false, false);
+//const lancheCompleto = new Lanche(true, true, true, true, true);
+//lancheSimples.show();
+//lancheCompleto.show();
+
+class LancheDirector {
+    static buildHamburguerGourmet() {
+        return new LancheBuilder()
+            .setPao("Brioche")
+            .setCarne("Costela")
+            .setQueijo("Mussarela")
+            .setSalada("Rucula")
+            .setMolho("Molho Stacker")
+            .build();
+    }
+
+    static buildWhopper() {
+        return new LancheBuilder()
+            .setPao("Pão com Gergelim")
+            .setCarne("Carne Bovina Grelhada")
+            .setQueijo("American cheese derretido")
+            .setSalada("Alface, tomate, cebola, picles ")
+            .setMolho("Maionese, ketchup")
+            .build();
+    }
+
+    static buildBigMac() {
+        return new LancheBuilder()
+            .setPao("Pão com Gergelim")
+            .setCarne("Carne Bovina")
+            .setQueijo("Americano")
+            .setSalada("Sem Picles")
+            .setMolho("Molho Escroto")
+            .build();
+    }
+}
+
+const hamburguerGourmet = LancheDirector.buildHamburguerGourmet();
+const whopper = LancheDirector.buildWhopper();
+const bigmac = LancheDirector.buildBigMac();
+
+
+hamburguerGourmet.show();
+whopper.show();
+bigmac.show();
