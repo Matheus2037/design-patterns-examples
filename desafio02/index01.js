adc6ed98f1c6bf4b6269f9fa429b066410ad17c5
Matheus Da Silva Gastaldi <matheus.30220@alunosatc.edu.br>
2025-10-29 19:36:17 -0300

    Subindo primeiro exercicio

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,43 @@
-console.log("Hello, World! 001");
+// BASE
+class Message {
+    constructor(text) {
+      this.text = text;
+    }
+  
+    getText() {
+      return this.text;
+    }
+} 
+
+// Decorator base
+class MessageDecorator {
+    constructor(message) {
+      this.message = message;
+    }
+  
+    getText(text) {
+      this.message.getText(text);
+    }
+}
+
+
+// Decorator concreto: envia mensagem no Whatsapp
+class WhatsappMessage extends MessageDecorator {
+    getText(text) {
+        super.getText(text);
+        console.log("Enviando mensagem no Whatsapp:", text);
+    }
+}
+
+// Decorator concreto: envia mensagem no Discord
+class DiscordMessage extends MessageDecorator {
+    getText(text) {
+        super.getText(text);
+        console.log("Enviando mensagem no Discord:", text);
+    }
+}
+
+
+// Cliente: empilha os decoradores
+const message = new WhatsappMessage(new DiscordMessage(new Message()));
+message.getText("Mensagem enviada com sucesso!");
